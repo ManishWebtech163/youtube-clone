feac3d208c3421693f75aa61846d9bbfc1d87d55
ManishWebtech163 <panchalmanish163@gmail.com>
2023-12-11 11:24:27 +0530

    packjson chnage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import jsconfigPaths from "vite-jsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/youtube-clone/",
   plugins: [react(), jsconfigPaths()],
 })
